d9a8d04a26c6f372fbea0b6b82591daa2b0d96d3
AvivTarkay <61126933+AvivTatkay@users.noreply.github.com>
2021-04-19 13:20:11 +0300

    made some changes on routing

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 			<div className="main-content">
 				<div className="content">
 					<Switch>
-						<Route path="/" exact>
+						<Route path="/my-latest-portfolio" exact>
 							<HomePage />
 						</Route>
 						<Route path="/about" exact>
